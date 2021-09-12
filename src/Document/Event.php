fc16fdcef1a0de6821e4325818215a41ae5283a7
Romitou <romitou@protonmail.com>
2021-09-12 23:14:56 +0200

    :sparkles: Add Event document

---
+++ src/Document/Event.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Document;
+
+use DateTime;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+
+/**
+ * Class Event
+ * @package App\Document
+ * @MongoDB\Document(collection="events")
+ */
+class Event
+{
+
+    /**
+     * @MongoDB\Id(type="string")
+     */
+    private string $id;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    private string $title;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    private string $shortDescription;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    private string $imageUrl;
+
+    /**
+     * @MongoDB\Field(type="date")
+     */
+    private DateTime $startDate;
+
+    /**
+     * @MongoDB\Field(type="date")
+     */
+    private DateTime $endDate;
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getShortDescription(): string
+    {
+        return $this->shortDescription;
+    }
+
+    /**
+     * @param string $shortDescription
+     */
+    public function setShortDescription(string $shortDescription): void
+    {
+        $this->shortDescription = $shortDescription;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImageUrl(): string
+    {
+        return $this->imageUrl;
+    }
+
+    /**
+     * @param string $imageUrl
+     */
+    public function setImageUrl(string $imageUrl): void
+    {
+        $this->imageUrl = $imageUrl;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getStartDate(): DateTime
+    {
+        return $this->startDate;
+    }
+
+    /**
+     * @param DateTime $startDate
+     */
+    public function setStartDate(DateTime $startDate): void
+    {
+        $this->startDate = $startDate;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getEndDate(): DateTime
+    {
+        return $this->endDate;
+    }
+
+    /**
+     * @param DateTime $endDate
+     */
+    public function setEndDate(DateTime $endDate): void
+    {
+        $this->endDate = $endDate;
+    }
+
+}
