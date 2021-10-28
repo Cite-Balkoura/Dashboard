4e2bda8305a234f17176d05e8104a45c1d654d99
Romitou <romitou@protonmail.com>
2021-10-28 21:33:25 +0200

    :sparkles: Add events' registration status

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Document\User;
+use App\Repository\Common\ParticipationRepository;
+use App\Repository\Common\ProfileRepository;
 use App\Repository\EventRepository;
 use App\Utils\TimeUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,10 +14,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class EventController extends AbstractController
 {
     #[Route('/events', name: 'events')]
-    public function index(EventRepository $eventRepository): Response
+    public function index(EventRepository $eventRepository, ProfileRepository $profileRepository, ParticipationRepository $participationRepository): Response
     {
+        $profile = null;
+        $participations = [];
+
+        /** @var User $user */
+        if ($user = $this->getUser()) {
+            if ($profile = $profileRepository->findByDiscordId(intval($user->getDiscordId()))) {
+                $participations = $participationRepository->findByProfileId(intval($profile->getId()));
+            }
+        }
+
         return $this->render('events/index.html.twig', [
-            'events' => $eventRepository->findAll()
+            'events' => $eventRepository->findAll(),
+            'canRegister' => null !== $profile,
+            'participations' => $participations
         ]);
     }
 
